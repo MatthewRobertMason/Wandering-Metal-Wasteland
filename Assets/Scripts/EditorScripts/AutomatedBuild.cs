38b1d6061f5be7abbfee033bb482d2380e6d8677
matt <matt@matthewmason.ca>
2017-07-27 21:15:10 -0300

    script update

--- Assets/Scripts/EditorScripts/AutomatedBuild.cs
+++ Assets/Scripts/EditorScripts/AutomatedBuild.cs
@@ -8,12 +8,6 @@ public class AutomatedBuild : MonoBehaviour
 {
     public static void BuildWebGL()
     {
-        //string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
-        string[] levels = new string[] { "Assets/Scene1.unity", "Assets/Scene2.unity" };
-
-        // Build player.
-        //BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
-        
         BuildPlayerOptions options;
         options.scenes = new string[] { "Assets/Scene1.unity" };
         options.targetGroup = BuildTargetGroup.WebGL;
