f1353ea72eab27abc189e5c6aa86c24082a6d1b9
matt <matt@matthewmason.ca>
2017-07-30 13:25:31 -0300

    fix the broken build

--- Assets/Editor/AutomatedBuild.cs
+++ Assets/Editor/AutomatedBuild.cs
@@ -9,7 +9,10 @@ public class AutomatedBuild : MonoBehaviour
     public static void BuildWebGL()
     {
         BuildPlayerOptions options;
-        options.scenes = new string[] { "Assets/Scene1.unity" };
+        options.scenes = new string[] { 
+            "Assets/MainMenu.unity",
+            "Assets/Level1.unity" 
+        };
         options.targetGroup = BuildTargetGroup.WebGL;
         options.target = BuildTarget.WebGL;
         options.locationPathName = ".\\Build\\WebGL";
