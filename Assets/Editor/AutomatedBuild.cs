462bca7d28ea884d058e05e0b879a1071682f9c0
matt <matt@matthewmason.ca>
2017-07-30 15:04:49 -0300

    revert stuff

--- Assets/Editor/AutomatedBuild.cs
+++ Assets/Editor/AutomatedBuild.cs
@@ -9,10 +9,11 @@ public class AutomatedBuild : MonoBehaviour
     public static void BuildWebGL()
     {
         BuildPlayerOptions options;
-        options.scenes = new string[] { 
-            "MainMenu.unity",
-            "Level1.unity" 
-        };
+        options.scenes = new string[] { "Assets/Scene1.unity" };
+        //options.scenes = new string[] { 
+        //    "MainMenu.unity",
+        //    "Level1.unity" 
+        //};
 
         options.targetGroup = BuildTargetGroup.WebGL;
         options.target = BuildTarget.WebGL;
